0e697cd62f6b864de102a6e25853063454e2d91f
Alexandre <alonsocabado@hotmail.com>
2021-05-05 18:46:05 +0200

    Non names

--- patrocinadores.py
+++ patrocinadores.py
@@ -1,12 +1,12 @@
-# patrocinadores = [
-#     {"_id": 0, "name": "A", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
-#     {"_id": 1, "name": "B", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
-#     {"_id": 2, "name": "C", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
-#     {"_id": 3, "name": "D", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
-#     {"_id": 4, "name": "E", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
-#     {"_id": 5, "name": "H", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
-#     {"_id": 6, "name": "F", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
-#     {"_id": 7, "name": "L", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
-#     {"_id": 8, "name": "G", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
-#     {"_id": 9, "name": "I", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
-# ]
+patrocinadores = [
+    # {"_id": 0, "name": "A", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
+    # {"_id": 1, "name": "B", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
+    # {"_id": 2, "name": "C", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
+    # {"_id": 3, "name": "D", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
+    # {"_id": 4, "name": "E", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
+    # {"_id": 5, "name": "H", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
+    # {"_id": 6, "name": "F", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
+    # {"_id": 7, "name": "L", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
+    # {"_id": 8, "name": "G", "href": "#", "img_src": "https://i.imgur.com/1h1Snmk.jpg"},
+    # {"_id": 9, "name": "I", "href": "https://vypgestion.es/", "img_src": "https://i.imgur.com/fAaVaQH.png"},
+]
